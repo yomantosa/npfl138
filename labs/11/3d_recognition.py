01487083afb0aa7bcca0558d49bf16409c6d7966
Hirokita <56084580+yomantosa@users.noreply.github.com>
2025-05-13 13:26:30 +0200

    3d_recog

--- labs/11/3d_recognition.py
+++ labs/11/3d_recognition.py
@@ -14,11 +14,49 @@ from npfl138.datasets.modelnet import ModelNet
 # TODO: Define reasonable defaults and optionally more parameters.
 # Also, you can set the number of threads to 0 to use all your CPU cores.
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", default=..., type=int, help="Batch size.")
-parser.add_argument("--epochs", default=..., type=int, help="Number of epochs.")
+parser.add_argument("--batch_size", default=32, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=20, type=int, help="Number of epochs.")
 parser.add_argument("--modelnet", default=20, type=int, help="ModelNet dimension.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
 parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+
+class Model3D(npfl138.TrainableModule):
+    def __init__(self, args: argparse.Namespace) -> None:
+        super().__init__()
+        self.feature_extractor = torch.nn.Sequential(
+            torch.nn.Conv3d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
+            torch.nn.MaxPool3d(kernel_size=2),
+            torch.nn.Conv3d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            torch.nn.MaxPool3d(kernel_size=2),
+            torch.nn.Conv3d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            torch.nn.MaxPool3d(kernel_size=2),
+            torch.nn.Flatten(),
+            torch.nn.Linear(in_features=128 * 2 * 2 * 2, out_features=512),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.5),
+            torch.nn.Linear(in_features=512, out_features=ModelNet.LABELS),
+        )
+
+    def forward(self, input_volume: torch.Tensor) -> torch.Tensor:
+        output_logits = self.feature_extractor(input_volume)
+        return output_logits
+
+
+class ModelNetDataset(npfl138.TransformedDataset):
+    def __init__(self, dataset: torch.utils.data.Dataset, resolution=20, training=True):
+        self.training = training
+        formatted_data = []
+
+        for sample_index in range(len(dataset)):
+            original_voxel = dataset.data['grids'][sample_index]
+            class_label = dataset.data['labels'][sample_index]
+
+            resized_voxel = np.resize(original_voxel, (resolution, resolution, resolution))
+            voxel_tensor = torch.tensor(resized_voxel, dtype=torch.float32).unsqueeze(0)
+
+            formatted_data.append((voxel_tensor, class_label))
+
+        self._dataset = formatted_data
 
 
 def main(args: argparse.Namespace) -> None:
@@ -36,8 +74,22 @@ def main(args: argparse.Namespace) -> None:
     # Load the data.
     modelnet = ModelNet(args.modelnet)
 
+    train = torch.utils.data.DataLoader(ModelNetDataset(modelnet.train, resolution=20, training=True), batch_size=args.batch_size, shuffle=True)
+    dev = torch.utils.data.DataLoader(ModelNetDataset(modelnet.dev, resolution=20, training=True), batch_size=args.batch_size, shuffle=False)
+    test = torch.utils.data.DataLoader(ModelNetDataset(modelnet.test, resolution=20, training=False), batch_size=args.batch_size, shuffle=False)
+
+    
+    device = torch.device("mps" if torch.mps.is_available() else "cpu")
+
     # TODO: Create the model and train it
-    model = ...
+    model = Model3D(args).to(device)
+    model.configure(
+        optimizer=torch.optim.Adam(model.parameters(), lr=1e-3),
+        loss=torch.nn.CrossEntropyLoss(),
+        logdir=args.logdir,
+    )
+    
+    model.fit(train, dev=dev, epochs=args.epochs)
 
     # Generate test set annotations, but in `args.logdir` to allow parallel execution.
     os.makedirs(args.logdir, exist_ok=True)
